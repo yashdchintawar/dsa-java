3c8e4ebb262e89cc32090d831a848c82fd31a6b0
yashdchintawar <yashchintawar31@gmail.com>
2022-02-18 18:46:18 +0530

    even odd code

---
+++ EvenOdd.java
@@ -0,0 +1,22 @@
+package com.basics;
+
+import java.util.Scanner;
+
+public class EvenOdd {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Enter number to check Even Or Odd");
+        int num = in.nextInt();
+
+        if (num % 2 == 0){
+            System.out.println("Number " + num + " is Even ");
+        }
+        else {
+            System.out.println("Number " + num + " is Odd ");
+
+        }
+
+    }
+
+}
